9fee8ba10ad7115e06f6a8a489ca1a3259e9a29c
Darik <s.sherimkulov@gmail.com>
2022-03-10 19:46:09 +0100

    'news'

---
+++ DJANGO-PROJECT D 6.4.3/simpleapp/templatetags/custom_filters.py
@@ -0,0 +1,29 @@
+from django import template
+import random
+
+
+register = template.Library()   # если мы не зарегистрируем наши фильтры,
+# то Django никогда не узнает, где именно их искать, и фильтры потеряются
+#  {% load custom_filters %}.
+@register.filter(name='multiply') # регистрируем наш фильтр под именем multiply,
+# чтоб django понимал, что это именно фильтр, а не простая функция
+def multiply(value, arg):  # первый аргумент здесь — это то значение, к которому надо применить фильтр,
+# второй аргумент это аргумент фильтра, т. е. примерно следующее будет в шаблоне value|multiply:arg
+   if isinstance(value, str) and isinstance(arg, int):
+      return str(value) * arg  # возвращаемое функцией значение — это то значение, которое подставится к нам в шаблон
+   else:
+      raise ValueError(f'Нельзя умножить {type(value)} на {type(arg)}')
+
+
+@register.filter(name='censor')
+def censor(value):
+   chars = ["*", "#", "%", "&", "?", "@"]
+   bad_words = ["блять", "бля", "сука", "далбаеб", "пидр", "пидарас", "гандон", "хуй", "пизда", "ебан"]
+   if isinstance(value, str):
+      value = value.lower().split(' ')
+      for word in value:
+         if word in bad_words:
+            temp = random.sample(chars, len(word))
+            replaced_word = ''.join(temp)
+            value = [x.replace(word, replaced_word) for x in value]
+      return ' '.join(map(str, value)).capitalize()
---
+++ DJANGO-PROJECT D 6.4.3/simpleapp/views.py
@@ -0,0 +1,47 @@
+from django.views.generic import ListView, DetailView
+from .models import Product, Category, News
+from datetime import datetime
+
+
+class ProductList(ListView):
+    model = Product  # указываем модель, объекты которой мы будем выводить
+    template_name = 'products.html'  # указываем имя шаблона, где будет лежать HTML,
+    # в котором будут все инструкции о том, как именно пользователю должны вывестись наши объекты
+    context_object_name = 'products'  # это имя списка, в котором будут лежать все объекты,
+    # его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
+    queryset = Product.objects.order_by('-id')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['value1'] = None
+        return context
+
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product'
+
+
+class NewsList(ListView):
+    model = News
+    template_name = 'news.html'
+    context_object_name = 'news'
+    queryset = News.objects.order_by('-id')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
+
+
+class NewsDetail(DetailView):
+    model = News
+    template_name = 'new.html'
+    context_object_name = 'new'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
